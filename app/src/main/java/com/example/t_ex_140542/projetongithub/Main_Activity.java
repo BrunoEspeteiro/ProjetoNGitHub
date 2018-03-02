9d14a1b7a114ee796ac91468405334f23fc7091a
t-ex-140542 <brunno.pereira@outlook.com>
2018-03-02 12:01:33 -0300

    Alterações no layout

--- app/src/main/java/com/example/t_ex_140542/projetongithub/Main_Activity.java
+++ app/src/main/java/com/example/t_ex_140542/projetongithub/Main_Activity.java
@@ -1,7 +1,12 @@
 package com.example.t_ex_140542.projetongithub;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class Main_Activity extends AppCompatActivity {
 
@@ -9,5 +14,34 @@ public class Main_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNav);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.recents:
+                        Toast toast = Toast.makeText(Main_Activity.this,"Clique para História", Toast.LENGTH_LONG);
+                        toast.setGravity(Gravity.CENTER_HORIZONTAL|Gravity.CENTER_VERTICAL,0,0);
+                        toast.show();
+
+                        break;
+                    case R.id.favorite:
+                        Toast toast1 = Toast.makeText(Main_Activity.this,"Clique para História", Toast.LENGTH_LONG);
+                        toast1.setGravity(Gravity.CENTER_HORIZONTAL|Gravity.CENTER_VERTICAL,0,0);
+                        toast1.show();
+
+                        break;
+                    case R.id.location:
+                        Toast toast2 = Toast.makeText(Main_Activity.this,"Clique para História", Toast.LENGTH_LONG);
+                        toast2.setGravity(Gravity.CENTER_HORIZONTAL|Gravity.CENTER_VERTICAL,0,0);
+                        toast2.show();
+
+                        break;
+                }
+                return true;
+            }
+        });
     }
 }
